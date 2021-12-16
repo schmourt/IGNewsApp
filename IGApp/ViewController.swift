3d16d612821ce566b2d895b95154201146016ad6
Courtney Langmeyer <courtneygl1993@gmail.com>
2021-12-16 18:45:38 -0500

    Initial Commit

---
+++ IGApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IGApp
+//
+//  Created by Courtney Langmeyer on 12/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
